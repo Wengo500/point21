2ed19f4c67ed81fb48d995a41aa493d403fff82e
Vlad Zobnin <vladopodobniy@gmail.com>
2020-09-27 11:34:24 +0300

    try add div2

--- script.js
+++ script.js
@@ -3,14 +3,29 @@ function work() {
     div.className = 'vertical';
     let div1 = document.createElement('div');
     div1.className = 'horizontal';
+    let div2 = document.createElement('div');
+    div2.className = 'mouseclick';
     document.body.appendChild(div);
     document.body.appendChild(div1);
+    document.body.appendChild(div2);
     document.addEventListener('mousemove', (e) => {
         let x = e.clientX;
         let y = e.clientY;
         div.style.marginLeft = `${x}px`;
         div1.style.marginTop = `${y}px`;
     });
+
+    document.addEventListener('mousedown', (e) => {
+        let a = e.clientX;
+        let q = a;
+        let b = e.clientY;
+        let d = b;
+        div2.style.marginLeft = `${a}px`;
+        div2.style.width = `${Math.abs(e.clientX - q)}px`;
+        div2.style.marginTop = `${b}px`;
+        div2.style.height = `${Math.abs(e.clientY - d)}px`;
+        console.log(q, d);
+    })
 }
 
 work();
